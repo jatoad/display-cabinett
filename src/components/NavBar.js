fdc2457ab77f30f5a619ad3ced7b2dcee85f39ac
jatoad <jasperhmca.todd@gmail.com>
2023-10-22 13:36:00 +0000

    updated app.module.css

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
-import logo from "../assets/logo192.png";
+import logo from "../assets/logo.jpg";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
 import { CurrentUserContext } from "../App";
