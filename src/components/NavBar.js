245d140887648048fe47877b54fd6ad05ac62897
jatoad <jasperhmca.todd@gmail.com>
2023-10-23 18:32:34 +0000

    added the ability to track the state of the menu and close it when the user clicks outside

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,12 +3,71 @@ import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/logo.jpg";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
-import { useCurrentUser } from "../contexts/CurrentUserContext";
+import {
+  useCurrentUser,
+  useSetCurrentUser,
+} from "../contexts/CurrentUserContext";
+import Avatar from "./Avatar";
+import axios from "axios";
+import useClickOutsideToggle from "../hooks/useClickOutsideToggle";
 
 const NavBar = () => {
   const currentUser = useCurrentUser();
+  const setCurrentUser = useSetCurrentUser();
 
-  const loggedInIcons = <>{currentUser?.username}</>;
+  const { expanded, setExpanded, ref } = useClickOutsideToggle();
+  
+  const handleSignOut = async () => {
+    try {
+      // await axios.post("dj-rest-auth/logout/");
+      await axios.get("/profiles/1");
+      setCurrentUser(null);
+    } catch (err) {
+      console.log(err);
+      // Add here for now as the above will fail
+      setCurrentUser(null);
+    }
+  };
+
+  const addDrawerIcon = (
+    <NavLink
+      className={styles.NavLink}
+      activeClassName={styles.Active}
+      to="/drawer/create"
+    >
+      <i className="far fa-plus-square"></i>Add Drawer
+    </NavLink>
+  );
+
+
+  const loggedInIcons = (
+    <>
+      {/* <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/feed"
+      >
+        <i className="fas fa-stream"></i>Feed
+      </NavLink>
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/liked"
+      >
+        <i className="fas fa-heart"></i>Liked
+      </NavLink> */}
+      <NavLink className={styles.NavLink} to="/" onClick={handleSignOut}>
+        <i className="fas fa-sign-out-alt"></i>Sign out
+      </NavLink>
+      <NavLink
+        className={styles.NavLink}
+        to={`/profiles/${currentUser?.profile_id}`}
+      >
+        <Avatar src={currentUser?.profile_image} text="Profile" height={40} />
+      </NavLink>
+    </>
+  );
+
   const loggedOutIcons = (
     // use JSX empty element
     <>
@@ -30,15 +89,25 @@ const NavBar = () => {
   );
 
   return (
-    <Navbar className={styles.NavBar} expand="md" fixed="top">
+    <Navbar
+      expanded={expanded}
+      className={styles.NavBar}
+      expand="md"
+      fixed="top"
+    >
       <Container>
         <NavLink to="/">
           <Navbar.Brand>
             <img src={logo} alt="logo" height="45" />
           </Navbar.Brand>
         </NavLink>
-
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        {currentUser && addDrawerIcon}
+        {/* Record the state of the manu */}
+        <Navbar.Toggle
+          ref={ref}
+          onClick={() => setExpanded(!expanded)}
+          aria-controls="basic-navbar-nav"
+        />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
             <NavLink
