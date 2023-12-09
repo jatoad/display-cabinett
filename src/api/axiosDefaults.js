64eaf6d4746e6556258da448506cc903a3491a8c
jatoad <jasperhmca.todd@gmail.com>
2023-12-09 16:08:33 +0000

    remove axios header settings

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,10 +3,10 @@ import axios from "axios"
 // local URL
 axios.defaults.baseURL = 'https://full-stack-django-0f48f57393c6.herokuapp.com/' 
 
-axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
-axios.defaults.headers.post['Access-Control-Allow-Origin'] = 'true'
-axios.defaults.headers['Access-Control-Allow-Headers'] = '*'
-axios.defaults.withCredentials = true
+// axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
+// // axios.defaults.headers.post['Access-Control-Allow-Origin'] = 'true'
+// // axios.defaults.headers['Access-Control-Allow-Headers'] = '*'
+// axios.defaults.withCredentials = true
 
 // define interceptor Request and Response
 export const axiosReq = axios.create();
