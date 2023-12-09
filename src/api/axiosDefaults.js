600b878507058377ae60e392406f5b43c1d922bb
jatoad <jasperhmca.todd@gmail.com>
2023-12-09 15:54:29 +0000

    update axios header defaults

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -5,13 +5,8 @@ axios.defaults.baseURL = 'https://full-stack-django-0f48f57393c6.herokuapp.com/'
 
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.defaults.headers.post['Access-Control-Allow-Origin'] = 'true'
-axios.defaults.headers.post['Access-Control-Allow-Headers'] = '*'
-
-axios.defaults.headers.withCredentials = true
-
-// response.setHeader("Access-Control-Allow-Credentials", "true");
-// response.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-// response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+axios.defaults.headers['Access-Control-Allow-Headers'] = '*'
+axios.defaults.withCredentials = true
 
 // define interceptor Request and Response
 export const axiosReq = axios.create();
