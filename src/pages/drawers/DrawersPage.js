e4b98d1ddf439ded078cdce1206609bc51f925b9
jatoad <jasperhmca.todd@gmail.com>
2023-12-10 19:13:46 +0000

    comment out profile data

--- src/pages/drawers/DrawersPage.js
+++ src/pages/drawers/DrawersPage.js
@@ -69,7 +69,7 @@ function DrawersPage({ message, filter = "" }) {
         )}
       </Col>
       <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
-        <p>Popular profiles for desktop</p>
+        {/* <p>Popular profiles for desktop</p> */}
       </Col>
     </Row>
   );
