54acc30151c6c0ecf59c78fdc3200f6fbb1b7b99
jatoad <jasperhmca.todd@gmail.com>
2023-12-11 07:00:26 +0000

    disable token refresh

--- src/contexts/CurrentUserContext.js
+++ src/contexts/CurrentUserContext.js
@@ -25,6 +25,7 @@ export const CurrentUserProvider = ({ children }) => {
         } else {
             try {
                 const { data } = await axios.get("dj-rest-auth/user/");
+                console.log('HM data ', data)
                 setCurrentUser(data);
             } catch (err) {
                 console.log(err);
@@ -37,49 +38,49 @@ export const CurrentUserProvider = ({ children }) => {
         handleMount();
     }, []);
 
-    // // This runs before the handleMount
-    useMemo(() => {
-    axiosReq.interceptors.request.use(
-        async (config) => {
-        try {
-            await axios.post("/dj-rest-auth/token/refresh/");
-        } catch (err) {
-            setCurrentUser((prevCurrentUser) => {
-            if (prevCurrentUser) {
-                history.push("/signin");
-            }
-            return null;
-            });
-            return config;
-        }
-        return config;
-        },
-        (err) => {
-        return Promise.reject(err);
-        }
-    );
+    // // // This runs before the handleMount
+    // useMemo(() => {
+    // axiosReq.interceptors.request.use(
+    //     async (config) => {
+    //     try {
+    //         await axios.post("/dj-rest-auth/token/refresh/");
+    //     } catch (err) {
+    //         setCurrentUser((prevCurrentUser) => {
+    //         if (prevCurrentUser) {
+    //             history.push("/signin");
+    //         }
+    //         return null;
+    //         });
+    //         return config;
+    //     }
+    //     return config;
+    //     },
+    //     (err) => {
+    //     return Promise.reject(err);
+    //     }
+    // );
 
-    axiosRes.interceptors.response.use(
-        // If no error return the response
-        (response) => response,
-        async (err) => {
-        if (err.response?.status === 401) {
-            try {
-            await axios.post("/dj-rest-auth/token/refresh/");
-            } catch (err) {
-            setCurrentUser((prevCurrentUser) => {
-                if (prevCurrentUser) {
-                history.push("/signin");
-                }
-                return null;
-            });
-            }
-            return axios(err.config);
-        }
-        return Promise.reject(err);
-        }
-    );
-    }, [history]);
+    // axiosRes.interceptors.response.use(
+    //     // If no error return the response
+    //     (response) => response,
+    //     async (err) => {
+    //     if (err.response?.status === 401) {
+    //         try {
+    //         await axios.post("/dj-rest-auth/token/refresh/");
+    //         } catch (err) {
+    //         setCurrentUser((prevCurrentUser) => {
+    //             if (prevCurrentUser) {
+    //             history.push("/signin");
+    //             }
+    //             return null;
+    //         });
+    //         }
+    //         return axios(err.config);
+    //     }
+    //     return Promise.reject(err);
+    //     }
+    // );
+    // }, [history]);
 
       
   return (
